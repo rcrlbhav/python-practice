56bcb45ed17d9a5621586b668603cb96b525a6d5
rcrlbhav <64437811+rcrlbhav@users.noreply.github.com>
2020-04-28 18:54:23 +0530

    Exercise 5 of https://pythonpractice.org
    In this exercise, 2 random number two digit lists are generated, whose lengths are given by the user.
    Once the lists are generated, the program compares these lists and generates a new list of common values.

---
+++ Exercise_5.py
@@ -0,0 +1,30 @@
+import random
+
+i1 = int(input("What should the length of the first list be? :"))
+i2 = int(input("What about the second list? :"))
+
+l1 = list(random.randrange(1,100) for i in range(i1))
+l2 = list(random.randrange(1,100) for i in range(i2))
+
+print("The first list is: ",l1)
+print("The second list is: ",l2)
+
+
+com = []
+
+for n in l1:
+    if n in l2 and n not in com:
+        com.append(n)
+
+if com:
+    print("The common numbers from the above lists are :",com)
+else:
+    print("There are no common values.")
+
+
+
+
+
+
+
+
